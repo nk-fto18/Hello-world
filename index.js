620b8f307f918c63abbac78c1395b572b1c20fca
nk-fto18 <rodriguezfranco128@gmail.com>
2020-06-25 19:49:46 -0300

    agrega index.html

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
 res.send('Hello world');
-
 });
 
-app.listen(3000);
+app.listen(port);
